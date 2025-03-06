fb9d5dac66216bebe64e8a6b609588b349c040d6
cagopa <cagdas96vkp@gmail.com>
2025-03-06 22:52:44 +0300

    Add initial implementation of Mesh and Painter classes; include CMake configuration and VSCode settings

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
     Eigen::VectorXd d;
     std::cout<<"Computing geodesic distance to vertex "<<vid<<"..."<<std::endl;
     igl::exact_geodesic(V,F,VS,FS,VT,FT,d);
+    
     // Plot the mesh
     Eigen::MatrixXd CM;
     igl::parula(Eigen::VectorXd::LinSpaced(21,0,1).eval(),false,CM);
@@ -76,38 +77,3 @@ int main(int argc, char *argv[])
   return viewer.launch();
 }
 
-
-
-// int main(int argc, char *argv[])
-// {
-//   // Inline mesh of a cube
-//   const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-//     0.0,0.0,0.0,
-//     0.0,0.0,1.0,
-//     0.0,1.0,0.0,
-//     0.0,1.0,1.0,
-//     1.0,0.0,0.0,
-//     1.0,0.0,1.0,
-//     1.0,1.0,0.0,
-//     1.0,1.0,1.0).finished();
-//   const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-//     0,6,4,
-//     0,2,6,
-//     0,3,2,
-//     0,1,3,
-//     2,7,6,
-//     2,3,7,
-//     4,6,7,
-//     4,7,5,
-//     0,4,5,
-//     0,5,1,
-//     1,5,7,
-//     1,7,3).finished();
-
-//   // Plot the mesh
-//   igl::opengl::glfw::Viewer viewer;
-//   viewer.data().set_mesh(V, F);
-//   viewer.data().set_face_based(true);
-//   viewer.launch();
-
-// }
